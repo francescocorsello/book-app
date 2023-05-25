5d48a2300a1d42103a8f9ee7151a87cecf03f22e
Francesco Corsello <francesco.corsello@gmail.com>
2023-05-25 17:19:12 +0200

    node sql create new data

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const db = mysql.createConnection({
 
 })
 
+// Express Middleware
+
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.json("Hello this is the backend!")
 })
@@ -19,7 +23,21 @@ app.get("/books", (req, res) => {
     const q = "SELECT * FROM books"
     db.query(q, (err, data) => {
         if (err) return res.json(err)
-        return res.json(data)
+        return res.json(data);
+    })
+})
+
+app.post("/books", (req, res) =>{
+    const q = "INSERT INTO  books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+    ]
+
+    db.query(q, [values], (err, data) =>{
+        if (err) return res.json(err)
+        return res.json("Book has been created successfully");
     })
 })
 
