2af64a4b712220c7d7309d892714fa38519039c4
Francesco Corsello <francesco.corsello@gmail.com>
2023-05-25 19:34:02 +0200

    Add new data to MySql Databased complete

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router';
 
 function Add() {
+    const[book, setBook] = useState({
+        title:"",
+        desc:"",
+        price: null,
+        cover:""
+    });
+
+    const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setBook((prev) => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleClick = async e => {
+    e.preventDefault()
+    try {
+        await axios.post("http://localhost:8801/books", book)
+        navigate("/")
+    } catch (err) {
+        console.log(err)
+    }
+  }
+
+  console.log(book)
   return (
-    <div>
-    <h1>Add</h1>
- </div>
+    <div className='form'>
+        <h1> Add a new book</h1>
+        <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+        <input type="text" placeholder='desc ' onChange={handleChange} name='desc'/>
+        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+
+        <button onClick={handleClick}>Add</button>
+    </div>
   )
 }
 
