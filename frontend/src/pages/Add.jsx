4487aebc98bf5074112f6f669fe880181f6bc2a9
Francesco Corsello <francesco.corsello@gmail.com>
2023-07-15 18:22:01 +0200

    deploy updat2e

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,44 +1,67 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
+const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const [error,setError] = useState(false)
 
-function Add() {
-    const[book, setBook] = useState({
-        title:"",
-        desc:"",
-        price: null,
-        cover:""
-    });
-
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setBook((prev) => ({...prev, [e.target.name]: e.target.value}))
-  }
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  const handleClick = async e => {
-    e.preventDefault()
+  const handleClick = async (e) => {
+    e.preventDefault();
     try {
-        await axios.post(process.env.REACT_APP_DB_ENDPOINT, book)
-        navigate("/")
+      await axios.post(process.env.REACT_APP_DB_ENDPOINT, book);
+      navigate("/");
     } catch (err) {
-        console.log(err)
+      console.log(err);
+      setError(true)
     }
-  }
+  };
 
-  console.log(book)
   return (
-    <div className='form'>
-        <h1> Add a new book</h1>
-        <input type="text" placeholder='title' onChange={handleChange} name='title'/>
-        <input type="text" placeholder='desc ' onChange={handleChange} name='desc'/>
-        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
-        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
-
-        <button className='formButton' onClick={handleClick}>Add</button>
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="Book title"
+        name="title"
+        onChange={handleChange}
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Book desc"
+        name="desc"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Book price"
+        name="price"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Book cover"
+        name="cover"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Add</button>
+      {error && "Something went wrong!"}
+      <Link to="/">See all books</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
