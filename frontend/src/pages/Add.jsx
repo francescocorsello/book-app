2472781037e89e9446f8ea3daf30aa1afead1164
Francesco Corsello <francesco.corsello@gmail.com>
2023-07-15 18:25:40 +0200

    deploy upd3

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -21,47 +21,24 @@ const Add = () => {
   const handleClick = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(process.env.REACT_APP_DB_ENDPOINT, book);
+      await axios.post("http://localhost:8800/books", book);
       navigate("/");
     } catch (err) {
       console.log(err);
       setError(true)
     }
   };
-
   return (
-    <div className="form">
-      <h1>Add New Book</h1>
-      <input
-        type="text"
-        placeholder="Book title"
-        name="title"
-        onChange={handleChange}
-      />
-      <textarea
-        rows={5}
-        type="text"
-        placeholder="Book desc"
-        name="desc"
-        onChange={handleChange}
-      />
-      <input
-        type="number"
-        placeholder="Book price"
-        name="price"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        placeholder="Book cover"
-        name="cover"
-        onChange={handleChange}
-      />
-      <button onClick={handleClick}>Add</button>
-      {error && "Something went wrong!"}
-      <Link to="/">See all books</Link>
-    </div>
-  );
-};
+    <div className='form'>
+        <h1> Add a new book</h1>
+        <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+        <input type="text" placeholder='desc ' onChange={handleChange} name='desc'/>
+        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
 
-export default Add;
+        <button className='formButton' onClick={handleClick}>Add</button>
+    </div>
+  )
+}
+
+export default Add
